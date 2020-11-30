c4a1d4fb54a16faf7e6fc3e67ed86ae9c648711d
Brian Demro <demrobrian@hotmail.com>
2020-11-30 10:35:43 -0600

    Some new things added

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import {Switch, Route} from 'react-router-dom';
+
+import Auth from './Components/Auth/Auth';
+import Feed from './Components/Feed/Feed';
+import Form from './Components/Form/Form';
+import Posts from './Components/Posts/Posts';
+import Comments from './Components/Comments/Comments';
+import FunStories from './Components/FunStories/FunStories';
+import Profile from './Components/Profile/Profile';
+
+export default (
+    <Switch>
+        <Route exact path='/' component={Auth}/>
+        <Route path='/feed' component={Feed}/>
+        <Route path='/post/:id' component={Posts}/>
+        <Route path='/comment/:id' component={Comments}/>
+        <Route path='/new' component={Form}/>
+        <Route path='/fun' component={FunStories}/>
+        <Route path='/profile' component={Profile}/>
+    </Switch>
+)
