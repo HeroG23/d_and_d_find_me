92de5fa30b88aa1fab480348ac0373306c89f317
Brian Demro <demrobrian@hotmail.com>
2020-12-07 11:58:12 -0600

    changed the db sql for posts and comments

--- db/comments/check_comments.sql
+++ db/comments/check_comments.sql
@@ -1,5 +1,5 @@
  -- seeing all the comments
-SELECT p.post_id, u.username, c.* FROM comments c
+SELECT p.*, u.*, c.* FROM comments c
 JOIN posts p ON p.post_id = c.post_id
 JOIN users u ON u.user_id = c.user_id
 WHERE c.post_id = $1
--- db/posts/check_posts.sql
+++ db/posts/check_posts.sql
@@ -1,4 +1,4 @@
 -- --finding all posts
-SELECT p.*, u.username FROM posts p
+SELECT p.*, u.* FROM posts p
 JOIN users u ON u.user_id = p.user_id
 ORDER BY p.post_id
