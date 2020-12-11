e750fcc2f3fad8da8a31bba340571b0f276ef546
Brian Demro <demrobrian@hotmail.com>
2020-12-11 13:20:21 -0600

    preventing refresh

--- src/Components/ContactUs/ContactUs.js
+++ src/Components/ContactUs/ContactUs.js
@@ -17,7 +17,8 @@ class Email extends Component {
         this.setState({ [name]: value })
       }
     
-      handleSend = () => {
+      handleSend = (e) => {
+        e.preventDefault();
         const { name, email, message, title} = this.state
         axios.post('/api/email', { name, email, message, title }).then(res => {
           this.setState({
@@ -39,7 +40,7 @@ class Email extends Component {
               <input style={styles.input} placeholder='name' type="text" name='name' value={name} onChange={this.handleInput} />
               <input style={styles.input} placeholder='email' type="text" name='email' value={email} onChange={this.handleInput} />
               <input style={styles.input} placeholder='message' type="text" name='message' value={message} onChange={this.handleInput} />
-              <button style={styles.button} onClick={this.handleSend}>Send</button>
+              <button style={styles.button} onClick={e => this.handleSend(e)}>Send</button>
             </div>
           </div>
         )
